3ae8295d250cb513b82c3e3d9024a70ef38472e0
Kye Duncan <kdun0020@student.monash.edu>
2026-07-02 15:45:11 +1000

    Co-authored-by: nickgnwn327 <nickgnwn327@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # SETUMetrics
 A joint collaboration that explores variation in Monash University SETU Evaluation forms
 nick was here
+
+
+Data sourced from Sai Kumar; https://github.com/saikumarmk/unit-scores-dashboard/tree/master
--- src/data_cleaning.R
+++ src/data_cleaning.R
@@ -1,5 +1,29 @@
 library(tidyverse)
-install.packages("jsonlite")  # only needed once
 library(jsonlite)
 
-#IOWEHFOWHEFOWEF
+data <- fromJSON("data/raw/data.json")
+
+data_split <- data |> 
+  unnest(c(I1, I2, I3, I4, I5, I6, I7, I8, I9, I10, I11, I12, I13), names_sep ="-") 
+
+#median dataset
+data_median <- data_split |> 
+  slice(seq(2, n(), by = 2))
+
+#mean dataset 
+data_mean <- data_split |> 
+  slice(seq(1, n(), by = 2))
+
+
+#formatting dataset
+data_median <-  as_tibble(data_median) |> 
+  select(-"agg_score")
+data_mean <-  as_tibble(data_mean) |> 
+  select(-"agg_score")
+
+
+
+
+##Export dataset
+write.csv(data_mean, "data/clean/data_mean.csv")
+write.csv(data_median, "data/clean/data_median.csv")
